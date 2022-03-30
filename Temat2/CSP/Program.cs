0c0131b13f8367f93c043419fb7d5bf9896a8cb4
Ernest Przybyl <ernestprzybyl@gmail.com>
2022-03-30 17:43:12 +0200

    add binary sollution, recreate project

---
+++ Temat2/CSP/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Temat2_CSP.Binary;
+
+char[][] binary6 = Utils.LoadFromFile("C:\\Users\\ErnestPrzybyl\\Desktop\\SztuczIntel\\Temat2_CSP\\binary-futoshiki_dane_v1.0\\binary_6x6");
+char[][] binary8 = Utils.LoadFromFile("C:\\Users\\ErnestPrzybyl\\Desktop\\SztuczIntel\\Temat2_CSP\\binary-futoshiki_dane_v1.0\\binary_8x8");
+char[][] binary10 = Utils.LoadFromFile("C:\\Users\\ErnestPrzybyl\\Desktop\\SztuczIntel\\Temat2_CSP\\binary-futoshiki_dane_v1.0\\binary_10x10");
+
+
+void Binary(char[][] data, string datasetName)
+{
+    Console.WriteLine("Rozwiązania dla " + datasetName);
+    BackTracking b = new(data);
+
+    ICollection<BackTracking> sollutions = b.Solve();
+
+    foreach (var s in sollutions)
+    {
+        Console.WriteLine(s);
+        Console.WriteLine("=============================");
+    }
+}
+
+void Futoshiki()
+{
+
+}
+
+Binary(binary6, "binary 6x6");
+Binary(binary8, "binary 8x8");
+Binary(binary10, "binary 10x10");
--- Temat2_CSP/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Temat2_CSP.Binary;
-
-Console.WriteLine("Hello, World!");
-
-char[][] a = Utils.LoadFromFile("C:\\Users\\ErnestPrzybyl\\googledrive\\Studia\\Semestr6\\lab\\SztuczIntel\\Temat2_CSP\\binary-futoshiki_dane_v1.0\\binary_8x8");
-
-Console.WriteLine("AA");
