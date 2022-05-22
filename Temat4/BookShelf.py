1cc03404c44dd0a12dbd1d93526d0553f65173ce
EP-coode <ernestprzybyl@gmail.com>
2022-05-22 15:14:51 +0200

    add model training

--- Temat4/BookShelf.py
+++ Temat4/BookShelf.py
@@ -1,38 +1,58 @@
 
-from typing import Callable
+from operator import ge
+from typing import Callable, Dict
 from Book import Book
 
 
 class BookShelf:
-  def __init__(self) -> None:
-    self.books: list[Book] = []
+    def __init__(self) -> None:
+        self.books: list[Book] = []
 
-  def add_book(self, book: Book):
-    self.books.append(book)
+    def add_book(self, book: Book):
+        self.books.append(book)
 
-  def get_genres_count(self) -> dict[str, int]:
-    genres_dict: dict[str, int] = {}
+    def get_genres_count(self) -> dict[str, int]:
+        genres_dict: dict[str, int] = {}
 
-    for book in self.books:
-      for genre in book.genres:
-        if genres_dict.get(genre) is None:
-          genres_dict[genre] = 0
-        else:
-          genres_dict[genre] += 1
+        for book in self.books:
+            for genre in book.genres:
+                if genres_dict.get(genre) is None:
+                    genres_dict[genre] = 1
+                else:
+                    genres_dict[genre] += 1
 
-    return genres_dict
+        return genres_dict
 
-  def remove_genres(self, genre: str):
-    for book in self.books:
-      if genre in book.genres:
-        book.genres.remove(genre)
+    def remove_genres(self, genre: str):
+        for book in self.books:
+            if genre in book.genres:
+                book.genres.remove(genre)
+                if len(book.genres) == 0:
+                    self.books.remove(book)
 
-  def remove_unvalid_books(self, book_validator: Callable[[Book], bool]):
-    books_to_remove: list[Book] = []
+    def remove_unvalid_books(self, book_validator: Callable[[Book], bool]):
+        books_to_remove: list[Book] = []
 
-    for book in self.books:
-      if not book_validator(book):
-        books_to_remove.append(book)
+        for book in self.books:
+            if not book_validator(book):
+                books_to_remove.append(book)
 
-    self.books = list(
-      filter(lambda book: book not in books_to_remove, self.books))
+        self.books = list(
+            filter(lambda book: book not in books_to_remove, self.books))
+
+    def merge_genres(self, sub_genre: str, genere: str):
+        for book in self.books:
+            if sub_genre in book.genres and genere in book.genres:
+                book.genres.remove(genere)
+
+    def get_books_grouped_by_category(self):
+        books: Dict[str, list[Book]] = {}
+
+        for book in self.books:
+            for genre in book.genres:
+                if genre not in books:
+                    books[genre] = [book]
+                else:
+                    books[genre].append(book)
+
+        return books
--- Temat4/processData.py
+++ Temat4/processData.py
@@ -1,7 +1,48 @@
 import json
+import numpy as np
 from BookShelf import BookShelf
 from bookValidators import books_containing_genres, description_longer_than, no_books_with_genres_count_more_than
 from dataLoader import load_all_books_from_file
+import os
+import shutil
+# from sklearn.feature_extraction.text import TfidfTransformer
+# from sklearn.model_selection import train_test_split
+
+import re
+from nltk.stem import WordNetLemmatizer
+# nltk.download('stopwords')
+# nltk.download('wordnet')
+# nltk.download('omw-1.4')
+
+stemmer = WordNetLemmatizer()
+
+
+def prepare_document(doc):
+   # Remove all the special characters
+    document = re.sub(r'\W', ' ', str(doc))
+
+    # remove all single characters
+    document = re.sub(r'\s+[a-zA-Z]\s+', ' ', document)
+
+    # Remove single characters from the start
+    document = re.sub(r'\^[a-zA-Z]\s+', ' ', document)
+
+    # Substituting multiple spaces with single space
+    document = re.sub(r'\s+', ' ', document, flags=re.I)
+
+    # Removing prefixed 'b'
+    document = re.sub(r'^b\s+', '', document)
+
+    # Converting to Lowercase
+    document = document.lower()
+
+    # Lemmatization
+    document = document.split()
+
+    document = [stemmer.lemmatize(word) for word in document]
+    document = ' '.join(document)
+
+    return document
 
 
 book_shelf: BookShelf = load_all_books_from_file("./dataset/booksummaries.txt")
@@ -9,23 +50,39 @@ book_shelf: BookShelf = load_all_books_from_file("./dataset/booksummaries.txt")
 print(f"Input records count - {len(book_shelf.books)}")
 print(f"Processing...")
 
-book_shelf.remove_unvalid_books(description_longer_than(100))
-book_shelf.remove_unvalid_books(no_books_with_genres_count_more_than(5))
+book_shelf.remove_unvalid_books(description_longer_than(300))
+book_shelf.remove_unvalid_books(no_books_with_genres_count_more_than(3))
+
+book_shelf.merge_genres('Speculative fiction', 'Fiction')
+book_shelf.merge_genres('Science Fiction', 'Fiction')
+book_shelf.remove_genres('Fiction')
+
 genres_cout = book_shelf.get_genres_count()
 genres_cout = sorted(genres_cout.items(),
                      key=lambda item: item[1], reverse=True)
+
 top_genres = list(map(lambda item: item[0], genres_cout[0:5]))
 not_qualified_genres = list(map(lambda item: item[0], genres_cout[5:-1]))
+
 book_shelf.remove_unvalid_books(books_containing_genres(top_genres))
 
 for g in not_qualified_genres:
-  book_shelf.remove_genres(g)
+    book_shelf.remove_genres(g)
 
-print(f"Output records count - {len(book_shelf.books)}")
-print("Saving...")
+book_shelf.remove_genres('Fairytale fantasy')
 
-with open("output.json", "w") as f:
-  books_dict = [book.__dict__ for book in book_shelf.books]
-  json_data = json.dumps(books_dict, indent=2)
-  f.write(json_data)
-  f.close()
+books_by_category = book_shelf.get_books_grouped_by_category()
+
+OUT_DIR = 'output'
+
+if os.path.exists(OUT_DIR):
+    shutil.rmtree(OUT_DIR)
+
+os.mkdir(OUT_DIR)
+
+for category in books_by_category.keys():
+    os.mkdir(f"{OUT_DIR}/{category}")
+    for book in books_by_category[category]:
+        with open(f"{OUT_DIR}/{category}/{book.id}", "w", encoding="UTF-8") as file:
+            file.write(prepare_document(book.description))
+            file.close()
